b9eece0b2f1d117a4d4f5a55e8a5ba8da514bc67
dream-swing <mjsun.mail@gmail.com>
2017-08-19 21:48:21 -0400

    Setting up TrelloAPI and GoogleCalendarAPI unit tests

---
+++ src/api/GoogleCalendarAPI.spec.ts
@@ -0,0 +1,10 @@
+import "mocha";
+import * as chai from "chai";
+
+let expect = chai.expect;
+
+describe("Google Calendar API", function() {
+	describe("#listSingleEventsInRange()", function() {
+		
+	});
+});
